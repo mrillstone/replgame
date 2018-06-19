7e05d526cef45002a95bba849547fc17e7f03fb1
Meaghan Rillstone <meaghan.rillstone@gmail.com>
2018-06-19 21:23:32 +0000

    initial commmit: adds shell to project

---
+++ game_code.rb
@@ -0,0 +1,63 @@
+print "Let's skate choose your board! Street, Hill, or Cruiser " 
+board = gets.chomp
+case board
+when "street"
+   puts "Rad, Let's hit the park"
+ when "hill"
+   puts "Gnarly Let's go mach five"
+ when "cruiser"
+   puts "Let's hit the beach"
+ else
+   puts "Sorry bro I did't get that"
+end
+ # second question
+   when "Rad, Let's hit the park"
+      puts "hope"
+   when "Gnarly Let's go mach five"
+      puts "this"
+   when "Let's hit the beach"
+       puts "works"
+end
+
+# Let's hit the park,
+# #         /  |      _O
+#           \O/      //\_
+#    O_/     |        L
+#   -+       |\`    ./ L,   \_O
+#    |\      |/o    ~o~o~    /\/
+#   .L_L,   ./o            |,L               /\0
+#  -~o~o~------------+     o\||     LO_/      /\_
+#                    |       \|      I       /\
+#                    |       o-    .IVL,    .L_L,
+#                    +-------------~o~o~----~o~o~----------
+#                    
+#                    
+#  Let's go mach five 
+#      __\_O
+#. 0\_/|  \
+#.  \\_/
+#.   0\>
+
+# Let's hit the beach!
+
+# image website http://ascii.co.uk/art/skateboard
+
+
+# if user_imput.include? "street"
+#     puts "Rad, let's hit the park"
+# elsif user_imput.include? "hill"
+#     puts "Gnarly, Let's go mach five"
+#     elsif user_imput.include? "cruiser"
+#     puts "Let's hit the beach"
+# end
+
+# case
+#  when user_imput = "street"
+#     puts "Rad, Let's the park"
+#   when user_imput = "hill"
+#     puts "Gnarly Let's go mach five"
+#   when user_imput = "cruiser"
+#     puts "Let's hit the beach"
+#   else
+#     puts "Sorry bro I did't get that"
+#   end
