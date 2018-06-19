d80f990eedc8881298ab4deb0943d6faed4124d1
Meaghan Rillstone <meaghan.rillstone@gmail.com>
2018-06-19 23:15:56 +0000

    update working level 1 & 2, pictures need improvement

--- game_code.rb
+++ game_code.rb
@@ -1,23 +1,64 @@
+
+
+# create script Variables
+# 
+# monsters = ["blah", "example whatevs", "your momma"]
+
 print "Let's skate choose your board! Street, Hill, or Cruiser " 
 board = gets.chomp
+
+# when (true)
+#     if (board.is_a? String)
+#         break
+#     else
+#         puts "Sorry bro I didn't get that"
+#         board = gets.chomp
+        
 case board
-when "street"
-   puts "Rad, Let's hit the park"
- when "hill"
-   puts "Gnarly Let's go mach five"
- when "cruiser"
-   puts "Let's hit the beach"
- else
-   puts "Sorry bro I did't get that"
-end
- # second question
-   when "Rad, Let's hit the park"
-      puts "hope"
-   when "Gnarly Let's go mach five"
-      puts "this"
-   when "Let's hit the beach"
-       puts "works"
-end
+     when "street"
+       puts "Rad, Let's hit the park"
+     when "hill"
+       puts "Gnarly Let's go mach five"
+     when "cruiser"
+       puts "Let's hit the beach"
+     else
+       puts "Sorry bro I didn't get that"
+       board = gets.chomp 
+    end  
+
+puts "enter shred and your choosen board"
+
+location = gets.chomp
+
+case location 
+     when "shred street"
+          puts " Let's hit the park,
+           /  |      _O
+           \O/      //\_
+    O_/     |        L
+   -+       |\`    ./ L,   \_O
+    |\      |/o    ~o~o~    /\/
+   .L_L,   ./o            |,L               /\0
+  -~o~o~------------+     o\||     LO_/      /\_
+                    |       \|      I       /\
+                    |       o-    .IVL,    .L_L,
+                    +-------------~o~o~----~o~o~----------" 
+     when "shred hill"
+          puts "#  Let's go mach five 
+                        __\_O
+                    0\_/|  \
+                     \\_/
+                      0\  "
+     when "shred cruiser"
+          puts " Let's hit the beach!
+                                
+                              _\/_ 
+     __,O                     //0\                          
+    / /\__                      |    
+   __/\                         |                            
+  \___/__/                      |                              
+___0__0________________________|_________________________"
+end 
 
 # Let's hit the park,
 # #         /  |      _O
@@ -39,8 +80,17 @@ end
 #.   0\>
 
 # Let's hit the beach!
+#                                
+#                              _\/_ 
+#     __,O                     //0\                          
+#    / /\__                      |    
+#   __/\                         |                            
+#  \___/__/                      |                              
+# ___0__0________________________|_________________________
 
-# image website http://ascii.co.uk/art/skateboard
+# 
+
+# 
 
 
 # if user_imput.include? "street"
